86b722fe1cbd0ddf1d4e763d50f2de35dbc6aa13
Anders Bak-Nyhus <anders.bak-nyhus@hotmail.com>
2021-06-23 11:21:30 +0200

    exam

--- exam2/main.c
+++ exam2/main.c
@@ -26,11 +26,11 @@ int main(void)
 	input2Array(Xdata,Ydata,"dataPts.txt");
 	
 	input2Array(Fdata,Fdata1,"dataVals.txt");
-
+	//The -3 comes from the program i used to make the txt files, which creates "invisible" data
 	int nx = sizeof(Xdata)-3;
 	int ny = sizeof(Ydata)-3;
-//	printf("%d\t%d\n",nx,ny);
 
+	//create matrix
 	gsl_matrix* F =gsl_matrix_alloc(nx,ny);
 	int k = 0;
 	for(int i = 0; i < nx; i += 1){
@@ -41,36 +41,8 @@ int main(void)
 	}
 
 
-	//create matrix for the function values
-/*	int n = 5;
-	gsl_matrix* F =gsl_matrix_alloc(n,n);
-	gsl_matrix_set(F,0,0,Fdata[0]);
-	gsl_matrix_set(F,1,0,Fdata[1]);
-	gsl_matrix_set(F,2,0,Fdata[2]);
-	gsl_matrix_set(F,3,0,Fdata[3]);
-	gsl_matrix_set(F,4,0,Fdata[4]);
-        gsl_matrix_set(F,0,1,Fdata[5]);
-        gsl_matrix_set(F,1,1,Fdata[6]);
-        gsl_matrix_set(F,2,1,Fdata[7]);
-        gsl_matrix_set(F,3,1,Fdata[8]);
-        gsl_matrix_set(F,4,1,Fdata[9]);
-        gsl_matrix_set(F,0,2,Fdata[10]);
-        gsl_matrix_set(F,1,2,Fdata[11]);
-        gsl_matrix_set(F,2,2,Fdata[12]);
-        gsl_matrix_set(F,3,2,Fdata[13]);
-        gsl_matrix_set(F,4,2,Fdata[14]);
-        gsl_matrix_set(F,0,3,Fdata[15]);
-        gsl_matrix_set(F,1,3,Fdata[16]);
-        gsl_matrix_set(F,2,3,Fdata[17]);
-        gsl_matrix_set(F,3,3,Fdata[18]);
-        gsl_matrix_set(F,4,3,Fdata[19]);
-        gsl_matrix_set(F,0,4,Fdata[20]);
-        gsl_matrix_set(F,1,4,Fdata[21]);
-        gsl_matrix_set(F,2,4,Fdata[22]);
-        gsl_matrix_set(F,3,4,Fdata[23]);
-        gsl_matrix_set(F,4,4,Fdata[24]);
-*/
-	//numbers needed in the sum below
+
+	//numbers needed in the sum below where a is the minimum in the data set and b is max
 	double a = -1; double b =1;
 	//sum over both x and y in steps of 0.1
 	for(double px = a; px <= b; px += 0.1){
